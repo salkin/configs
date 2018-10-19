4e886f7c4cacf7bb3e99c8ef5cbaa3d2b8595b4c
Niklas Wik <niklas.wik@nokia.com>
2018-10-19 17:57:40 +0300

    Added tmux conf
    Signed-off-by: Niklas Wik <niklas.wik@nokia.com>

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 ln -s ${PWD}/vim/vimrcs ~/.vim_runtime
 ln -s ${PWD}/vim/.vimrc ~/.vimrc
 ln -s ${PWD}/vim/my_configs.vim ~/.vim_runtime/my_configs.vim
+ln -s ${PWD}/tmux.conf ~/.tmux.conf
